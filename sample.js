6d4d1edfd445dbd4d92ec1960c1fabe556d54d21
ouprogramming <ouprogramming@gmail.com>
2023-04-11 11:50:26 +0900

    1

---
+++ sample.js
@@ -0,0 +1,141 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '520896855560-vs8rd8anucp4cai0dkc6coh7qacpsfsf.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDtGTLYBZgWGH6C2DvsmNc4Y95xXaBJuz4';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+var item = []
+var variation = []
+var size = []
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    listFiles();
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+/**
+ * Print files.
+ */
+function listFiles() {
+  gapi.client.drive.files.list({
+    'pageSize': 10,
+    'fields': "nextPageToken, files(id, name)"
+  }).then(function(response) {
+    appendPre('Files:');
+    var files = response.result.files;
+    if (files && files.length > 0) {
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        appendPre(file.name + ' (' + file.id + ')');
+      }
+    } else {
+      appendPre('No files found.');
+    }
+  });
+}
+
+var select_item = document.querySelector('[name="item"]')
+var select_variation = document.querySelector('[name="variation"]')
+var select_size = document.querySelector('[name="size"]')
+
+function item(){
+  for (var i = 0; i < items.length; i++) {
+    var option = document.createElement("option")
+    option.text = variation[i]
+    option.value = variation[i]
+    select_item.variation.appendChild(option)
+  }
+}
+//バリエーション
+
+select_item.onchange = event =>{
+  for (var i = 0; i < variation.length; i++) {
+    var option = document.createElement("option")
+    option.text = variation[i]
+    option.value = variation[i]
+    select.variation.appendChild(option)
+  }
+}
+
+//サイズ
+select_variation.onchange = event =>{
+  for (var i = 0; i < size.length; i++) {
+    var option = document.createElement("option")
+    option.text = size[i]
+    option.value = size[i]
+    select.variation.appendChild(size)
+  }
+}
